34834e3b011e29282f5129337a618e05f25a7c35
Kink0xo <aryan.singh4565@gmail.com>
2024-08-08 13:18:06 +0530

    toastify'd the app

--- frontend/src/assets/problemPageFiles/problemPage.jsx
+++ frontend/src/assets/problemPageFiles/problemPage.jsx
@@ -38,68 +38,77 @@ function Problem() {
     }
   };
   return (
-    <div className="all-details-problem">
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        limit={1}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-        transition:Bounce
-      />
+    <div>
       <div>
-        <div className="nav-bar">
-          <AiFillHome className="home-button" onClick={handleHome} />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          limit={1}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          transition:Bounce
+        />
+      </div>
+      <div className="all-details-problem">
+        <div>
+          <div className="nav-bar">
+            <AiFillHome className="home-button" onClick={handleHome} />
 
-          <button value="problems" onClick={()=>{navigate("/problems");}}>
-            Problem List
-          </button>
+            <button
+              value="problems"
+              onClick={() => {
+                navigate("/problems");
+              }}
+            >
+              Problem List
+            </button>
 
-          <button value="addproblem" onClick={(e) => handleAdminCheck(e)}>
-            Add a problem
-          </button>
+            <button value="addproblem" onClick={(e) => handleAdminCheck(e)}>
+              Add a problem
+            </button>
 
-          <button value="updateproblem" onClick={(e) => handleAdminCheck(e)}>
-            Edit a problem
-          </button>
+            <button value="updateproblem" onClick={(e) => handleAdminCheck(e)}>
+              Edit a problem
+            </button>
 
-          <button value="deleteproblem" onClick={(e) => handleAdminCheck(e)}>
-            Delete a problem
-          </button>
-        </div>
-        <h1 className="title-problem">
-          Problems List: &#40;Click on SOLVE to attempt the problem!&#41;
-        </h1>
-        <div className="instruction-list">
-          <p>
-            When adding a problem please make sure to abide by the following
-            instructions:-
-          </p>
-          <ul>
-            <li>
-              Please provide 1 sample test-case and 5 hidden test-cases for
-              strongly checking solutions
-            </li>
-            <li>
-              The time and memory limits are [1 sec] and [256 Mb] respectively
-            </li>
-          </ul>
-        </div>
-        <div className="problem-list">
-          <ol>
-            {problemList.map((problemName, index) => (
-              <li key={problemName}>
-                {problemName}
-                <button onClick={() => handleSolve(index)}>SOLVE</button>
+            <button value="deleteproblem" onClick={(e) => handleAdminCheck(e)}>
+              Delete a problem
+            </button>
+          </div>
+          <h1 className="title-problem">
+            Problems List: &#40;Click on SOLVE to attempt the problem!&#41;
+          </h1>
+          <div className="instruction-list">
+            <p>
+              When adding a problem please make sure to abide by the following
+              instructions:-
+            </p>
+            <ul>
+              <li>
+                Please provide 1 sample test-case and 5 hidden test-cases for
+                strongly checking solutions
               </li>
-            ))}
-          </ol>
+              <li>
+                The time and memory limits are [1 sec] and [256 Mb] respectively
+              </li>
+            </ul>
+          </div>
+          <div className="problem-list">
+            <ol>
+              {problemList.map((problemName, index) => (
+                <li key={problemName}>
+                  {problemName}
+                  <button onClick={() => handleSolve(index)}>SOLVE</button>
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
       </div>
     </div>
